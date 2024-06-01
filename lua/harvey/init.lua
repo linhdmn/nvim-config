eee75ffa5314c1d69c407e068b9b8b7b129cfe8c
Linh. Doan Minh Nhat <linh.doan@bluesg.com.sg>
2024-06-01 22:30:58 +0700

    Feat: rename themprimeagen to my name harvey && add git signs

--- lua/harvey/init.lua
+++ lua/harvey/init.lua
@@ -1,6 +1,6 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
-require("theprimeagen.lazy_init")
+require("harvey.set")
+require("harvey.remap")
+require("harvey.lazy_init")
 
 -- DO.not
 -- DO NOT INCLUDE THIS
--- lua/harvey/lazy_init.lua
+++ lua/harvey/lazy_init.lua
@@ -12,6 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "theprimeagen.lazy",
+    spec = "harvey.lazy",
     change_detection = { notify = false }
 })
